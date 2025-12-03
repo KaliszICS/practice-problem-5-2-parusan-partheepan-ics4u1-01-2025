aa34832f0f40a1b1d0f8f3dbbdcc74c6f10bb8ce
James Kalisz <james.kalisz@tdsb.on.ca>
2025-12-03 13:02:21 -0500

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -2,5 +2,10 @@
 class Notes {
 	public static void main(String[] args) {
 
+		Rectangle rectangle = new Rectangle(0, 0);
+		RectPrism rectPrism = new RectPrism(2, 2, 2);
+
+		System.out.println(rectangle.area());
+		System.out.println(rectPrism.area());
 	}
 }
---
+++ src/main/java/RectPrism.java
@@ -0,0 +1,38 @@
+public class RectPrism extends Rectangle {
+
+    private int height;
+    // private int length;
+
+
+
+    public RectPrism(int length, int width, int height) {
+        //must be the first line of your constructor
+        super(length, width); //must run your superclass constructor
+        this.length = length;
+        // super.length = 5;
+        // this.width = width;
+        this.height = height;
+        //length -> local variable (the one that was created in this method/constrcutor)
+        //length -> this.length -> instance variable
+        //length -> super.length -> superclasses instance variable
+    }
+
+    //variables used in our methods and constructors
+    //check for local -> check for "this" (instance variables) -> check the superclass for an instance variable
+
+    public int getSuperLength() {
+        return super.length;
+    }
+
+    public int getThisLength() {
+        return this.length;
+    }
+
+    //area method already exists
+    @Override
+    public int area() {
+        return this.height * this.width * this.length;
+        //Yes i know this is volume
+    }
+
+}
